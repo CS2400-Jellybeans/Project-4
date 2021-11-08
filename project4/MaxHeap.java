1e675187292a49475ea9b9e810e50f50ce33e92e
pesimelaki1 <49598484+pesimelaki1@users.noreply.github.com>
2021-11-08 15:26:15 -0800

    added reheap method

--- project4/MaxHeap.java
+++ project4/MaxHeap.java
@@ -77,7 +77,32 @@ public final class MaxHeap<T extends Comparable<? super T>>
 
    public void reheap(int rootIndex)
    {
-   // See Slide 25.
+      boolean done = false;
+      T orphan = heap[rootIndex];
+      int leftChildIndex = 2 * rootIndex + 1;
+   
+      while (!done && (leftChildIndex <= lastIndex))
+      {
+         int largerChildIndex = leftChildIndex;
+         int rightChildIndex = leftChildIndex + 1;
+   
+         if ( (rightChildIndex <= lastIndex) &&
+               heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+         {
+            largerChildIndex = rightChildIndex;
+         } // end if
+   
+         if (orphan.compareTo(heap[largerChildIndex]) < 0)
+         {
+            heap[rootIndex] = heap[largerChildIndex];
+            rootIndex = largerChildIndex;
+            leftChildIndex = 2 * rootIndex + 1;
+         }
+         else
+            done = true;
+      } // end while
+   
+      heap[rootIndex] = orphan;
    } // end reheap
 
    public boolean isEmpty()
